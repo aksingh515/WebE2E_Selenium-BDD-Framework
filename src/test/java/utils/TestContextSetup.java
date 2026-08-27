2721862d5c6b0a64427e5ccaafbbb0790b3a08d6
aksingh515 <79060694+aksingh515@users.noreply.github.com>
2026-08-28 00:03:57 +0530

    Initialize WebDriver in TestContextSetup

--- src/test/java/utils/TestContextSetup.java
+++ src/test/java/utils/TestContextSetup.java
@@ -1,17 +1,19 @@
 package utils;
 
 import java.util.HashMap;
+import org.openqa.selenium.WebDriver;
 
 import pomPages.PageObjectManager;
 
 public class TestContextSetup {
-	
+	public WebDriver driver ;
 	public HashMap<String, String> data;
 	public PageObjectManager pageObjectManager;
 	
 	public TestContextSetup() 
 	{
-		pageObjectManager = new PageObjectManager(DriverFactory.getDriver());
+		driver = DriverFactory.getDriver();
+		pageObjectManager = new PageObjectManager(driver);
 	}
 
 }
